030c74757167f51b87c41b44cdd1cd953b3deaf9
Alex Eden <alexandereden91@gmail.com>
2019-11-07 22:43:33 -0600

    Eyyy it uploads

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "Adafruit_seesaw.h"
 #include "FeatherJoyWing.h"
+
+Adafruit_SSD1306 *display = new Adafruit_SSD1306(128, 32, &Wire);
 
 void setup() {
   // put your setup code here, to run once:
